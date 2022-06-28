a6985357cedb11329c599fe5a3e38447d078288a
Corentin Renard <corentin@microdon.org>
2022-06-28 20:56:10 +0200

    feat: add dev database

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,5 +1,25 @@
 import express, { Application, Request, Response, NextFunction } from "express"
 import bodyParser from "body-parser"
+import { Pool } from "pg";
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT || "5432")
+});
+
+
+const connectToDB = async () => {
+    try {
+        await pool.connect();
+        console.log('cconnecté');
+    } catch (err) {
+        console.log(err);
+    }
+    };
+
+connectToDB();
 
 const app: Application = express()
 
@@ -7,7 +27,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("TS App is Running")
+    res.send("TS App is Running, enfin ffs")
 })
 const PORT = 3000
 
